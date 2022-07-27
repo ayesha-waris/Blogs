3968e2399ababf4ebe66ae83059c15e9a429c26c
ayeshoali <ayeshoali@gmail.com>
2022-07-27 18:50:24 +0500

    editt button added on blog detail page

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -2,9 +2,12 @@ import Wrapper from "./Wrapper";
 import classes from './BlogDetail.module.css';
 import Blog from "./Blog";
 const BlogDetail = () => {
+  const clickHandler = () => {
+    console.log("hi");
+  }
   return ( 
   <Wrapper>
-    <button> Edit</button>
+    <button onClick= {clickHandler} className={classes.edit}> Edit</button>
    <Blog className={classes.detail} noButton = {true}/>
    
   </Wrapper>
