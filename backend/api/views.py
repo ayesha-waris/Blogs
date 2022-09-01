9363037553ee4701ef63cd1b9e9521ff85385262
ayeshoali <ayeshoali@gmail.com>
2022-09-01 18:38:41 +0500

    chore: permission classes specified for logs APIs

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,12 @@
 from rest_framework import generics
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 from .models import BlogsModel
 from .serializers import BlogSerializer
 
 # Create your views here.
 
 class BlogCreateApiView(generics.CreateAPIView):
+    permission_classes=[IsAuthenticated]
     queryset = BlogsModel.objects.all()
     serializer_class = BlogSerializer 
     def perform_create(self, serializer):
@@ -15,19 +16,23 @@ class BlogCreateApiView(generics.CreateAPIView):
 class BlogListApiView(generics.ListAPIView):
     queryset = BlogsModel.objects.all()
     serializer_class = BlogSerializer 
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class BlogDetailApiView(generics.RetrieveAPIView):
     queryset = BlogsModel.objects.all()
     serializer_class = BlogSerializer 
     lookup_field = 'pk'
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BlogUpdateApiView(generics.UpdateAPIView):
     queryset = BlogsModel.objects.all()
     serializer_class = BlogSerializer 
     lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
 
 class BlogDeleteApiView(generics.DestroyAPIView):
     queryset = BlogsModel.objects.all()
     serializer_class = BlogSerializer 
     lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
